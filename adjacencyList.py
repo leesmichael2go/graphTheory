fab125ab97736f73feb16fbc728b16a819617f31
Michael S Lee <leesmichael2go@gmail.com>
2015-09-01 19:19:58 -0400

    Update adjacencyList.py

--- adjacencyList.py
+++ adjacencyList.py
@@ -9,7 +9,7 @@ class Vertex:
     def __str__(self):
         return str(self.id + ' connectedTo: ' + str([x.id for x in self.connectedTo]))
     
-    def get Connections(self):
+    def getConnections(self):
         return self.connectedTo.keys()
     
     def getId(self):
@@ -24,7 +24,7 @@ class Graph:
         self.numVertices = 0
      
     def addVertex(self,key):
-        self.numVertices = self.Vertices + 1
+        self.numVertices = self.numVertices + 1
         newVertex = Vertex(key)
         self.vertList[key] = newVertex
         return newVertex
@@ -51,4 +51,20 @@ class Graph:
     def __iter__(self):
         return iter(self.vertList.values())
     
-            
+g = Graph()
+for i in range(6):
+    g.addVertex(i)
+g.addEdge(0,1,5)
+g.addEdge(0,5,2)
+g.addEdge(1,2,4)
+g.addEdge(2,3,9)
+g.addEdge(3,4,7)
+g.addEdge(3,5,3)
+g.addEdge(4,0,1)
+g.addEdge(5,4,8)
+g.addEdge(5,2,1)
+
+
+for v  in g:
+    for w in v.getConnections():
+        print("( %s , %s )" % (v.getId(), w.getId()))
